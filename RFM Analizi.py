03d19f458782966b6d3e7bdb054161fd074f3b6e
Ayberk Dönmez <121521651+AyberkDnmez@users.noreply.github.com>
2023-07-12 23:36:45 +0300

    Add files via upload

--- RFM Analizi.py
+++ RFM Analizi.py
@@ -50,13 +50,13 @@ df.isnull().sum()
 # eşsiz ürün sayısı nedir ?
 df["Description"].nunique()
 
-# hangi ürün kaç kere satışa konu olmuştur ? (bu kısmı iyi anlamadım.)
+# hangi ürün kaç kere satışa konu olmuştur ?
 df["Description"].value_counts().head()
 
 # hangi üründen kaçar tane satılmıştır ?
 # aşağıdaki kodda quantityler eksi değerde gözükmektedir. veri ön işleme kısmında bu problem ortadan kaldırılacaktır.
 
-df.groupby("Description").agg({"Quantity":"sum"}).head()
+df.groupby("Description").agg({"Quantity": "sum"}).head()
 
 # ana amacımıza dönelim ve quantitylere göre küçükten büyüğe doğru sıralayalım.
 
@@ -166,11 +166,11 @@ rfm["monetary_score"] = pd.qcut(rfm["monetary"], 5, labels=[1, 2, 3, 4, 5])
 
 # Frequencyi de aynı şekilde çalıştıralım. (Hata alacağız.)
 
-rfm["frequency_score"] = pd.qcut(rfm["frequency"], 5, labels=[1, 2, 3, 4, 5])
+# rfm["frequency_score"] = pd.qcut(rfm["frequency"], 5, labels=[1, 2, 3, 4, 5])
 
 # Karşımıza çıkan hata : Oluşturulan aralıklarda unique değerler yer almamaktadır. Yani o kadar fazla tekrar eden bir frekans var ki küçükten büyüğe sıralandığında çeyrek değerlere düşen değerler aynı olmuş. Bu problemi çözmek için rank metodunu kullanıyoruz. (İlk gördüğünü ilk sınıfa ata bilgisini veriyoruz.)
 
-rfm["frequency_score"] = pd.qcut(rfm["frequency"].rank(method="first"), 5, labels = [1, 2, 3, 4, 5])
+rfm["frequency_score"] = pd.qcut(rfm["frequency"].rank(method="first"), 5, labels=[1, 2, 3, 4, 5])
 
 # Şimdiki aşamada r ve f değerlerini bir araya getirerek yazmamız gerekmektedir.
 
@@ -185,3 +185,121 @@ rfm[rfm["RFM_SCORE"] == "55"]
 # Başka bir müşteri sınıfına bakalım
 
 rfm[rfm["RFM_SCORE"] == "11"]
+
+# 6. RFM Segmentlerinin Oluşturulması ve Analiz Edilmesi (Creating & Analysing RFM Segments)
+
+# Nümerik anlamda değerlendirmeler yaptık. Bunların karşısına okunabilirliği artırmak adına bu segmentlerin isimlerini yazabiliriz.
+
+# Bunun için regex kullanacağız. Ancak şu aşamada regexin kendisi konumuzun içinde değil.
+
+# RFM İsimlendirmesi
+
+seg_map = {
+    r"[1-2][1-2]": "hibernating",
+    r"[1-2][3-4]": "at_Risk",
+    r"[1-2]5": "cant_loose",
+    r"3[1-2]": "about_to_sleep",
+    r"33": "need_attention",
+    r"[3-4][4-5]": "loyal_customers",
+    r"41": "promising",
+    r"51": "new_customers",
+    r"[4-5][2-3]": "potential_loyalists",
+    r"5[4-5]": "champions"
+}
+
+# Çok basit bir kod yazarak diyeceğiz ki yukarıdaki ifade regexi ifade etmektedir.
+
+rfm["segment"] = rfm["RFM_SCORE"].replace(seg_map, regex=True)
+
+# Segmentleri oluşturduk. Şimdi segmentlerin analizini yapmamız gerekmektedir. Örneğin bu sınıflardaki kişilerin recency ortalamaları nedir ?
+
+rfm[["segment", "recency", "frequency", "monetary"]].groupby("segment").agg(["mean", "count"])
+
+# need_attention sınıfıyla ilgilenmek istediğimizi düşünelim.
+
+rfm[rfm["segment"] == "need_attention"].head()
+
+# cant_loose a erişmek için:
+
+rfm[rfm["segment"] == "cant_loose"].head()
+
+# new_customers sınıfındakilerin idlerine erişmek istediğimizi düşünelim.
+
+rfm[rfm["segment"] == "new_customers"].index
+
+# yeni müşterilerin idlerini ilgili departmanlara yeni bir dataframele göndermek istediğimizi düşünelim.
+
+new_df = pd.DataFrame()
+new_df["new_customer_id"] = rfm[rfm["segment"] == "new_customers"].index
+
+# idleri integera çevirelim.
+
+new_df["new_customer_id"] = new_df["new_customer_id"].astype(int)
+
+# İlgili departmana bunu iletmek için excel ya da csv formatına çevirmemiz gereklidir. Çalışmamızı csv haline getirip dışarı çıkaralım.
+
+new_df.to_csv("new_customers.csv")
+
+# rfm çalışmamızı dışarı çıkaralım.
+
+rfm.to_csv("rfm.csv")
+
+# 7. Tüm Sürecin Fonksiyonlaştırılması
+
+def create_rfm(dataframe, csv=False):
+
+    # VERIYI HAZIRLAMA
+    dataframe["TotalPrice"] = dataframe["Quantity"] * dataframe["Price"]
+    dataframe.dropna(inplace=True)
+    dataframe = dataframe[~dataframe["Invoice"].str.contains("C", na=False)]
+
+    # RFM METRIKLERININ HESAPLANMASI
+    today_date = dt.datetime(2011, 12, 11)
+    rfm = dataframe.groupby("Customer ID").agg({"InvoiceDate": lambda date: (today_date - date.max()).days,
+                                                "Invoice": lambda num: num.nunique(),
+                                                "TotalPrice": lambda price: price.sum()})
+
+    rfm.columns = ["recency", "frequency", "monetary"]
+    rfm = rfm[(rfm["monetary"] > 0)]
+
+    # RFM SKORLARININ HESAPLANMASI
+    rfm["recency_score"] = pd.qcut(rfm["recency"], 5, labels=[5, 4, 3, 2, 1])
+    rfm["frequency_score"] = pd.qcut(rfm["frequency"].rank(method="first"), 5, labels=[1, 2, 3, 4, 5])
+    rfm["monetary_score"] = pd.qcut(rfm["monetary"], 5, labels=[1, 2, 3, 4, 5])
+
+    # cltv_df skorları kategorik değere dönüştürülüp df'e eklendi
+    rfm["RFM_SCORE"] = (rfm["recency_score"].astype(str) +
+                        rfm["frequency_score"].astype(str))
+
+    # SEGMENTLERIN ISIMLENDIRILMESI
+    seg_map = {
+        r"[1-2][1-2]": "hibernating",
+        r"[1-2][3-4]": "at_risk",
+        r"[1-2]5": "cant_loose",
+        r"3[1-2]": "about_to_sleep",
+        r"33": "need_attention",
+        r"[3-4][4-5]": "loyal_customers",
+        r"41": "promising",
+        r"51": "new_customers",
+        r"[4-5][2-3]": "potential_loyalists",
+        r"5[4-5]": "champions"
+    }
+
+    rfm["segment"] = rfm["RFM_SCORE"].replace(seg_map, regex=True)
+    rfm = rfm[["recency", "frequency", "monetary", "segment"]]
+    rfm.index = rfm.index.astype(int)
+
+    if csv:
+        rfm.to_csv("rfm.csv")
+
+    return rfm
+
+df = df_.copy()
+
+rfm_new = create_rfm(df, csv=True)
+
+# Daha neler yapılabilir ?
+
+# 1.si fonksiyondaki alt özellikler parçalanabilir. Yani bu veri hazırlama, rfm metriklerinin hazırlanması gibi başlıklar için ayrı ayrı fonksiyonlar yazılabilir. Bu fonksiyonlar ayrı ayrı yazıldıktan sonra ara işlemlere müdahale etme şansımız olur. (Bir fonksiyondaki çıktı diğer fonksiyona girdi olacak şekilde.)
+
+# Bu analiz dönem dönem tekrar edilebilir. Örneğin bu analizi yaptıktan belirli bir süre sonra mevzu bahis müşterilerden bazıları artık müşteri olmayabilir. Dolayısıyla buradaki değişimleri gözlemlemek oldukça kritik ve önemlidir. Çünkü takip etme süreci günümüzde şirketler için önemli bir problem oluşturmaktadır.
